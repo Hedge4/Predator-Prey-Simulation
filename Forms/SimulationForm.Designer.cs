2abea7b4190b296d571ce1463ff3e8904a8aa07e
Hedde van Heerde <heddevanheerde4@gmail.com>
2026-02-23 05:11:19 +0100

    Add separator lines between grass/prey/predator settings

--- Forms/SimulationForm.Designer.cs
+++ Forms/SimulationForm.Designer.cs
@@ -35,6 +35,8 @@ namespace Predator_Prey_Simulation {
             this.label1 = new System.Windows.Forms.Label();
             this.textBox0 = new System.Windows.Forms.TextBox();
             this.label0 = new System.Windows.Forms.Label();
+            this.separatorLine1 = new System.Windows.Forms.Panel();
+            this.separatorLine2 = new System.Windows.Forms.Panel();
             this.graphicsPanel = new Predator_Prey_Simulation.DoubleBufferedPanel();
             this.mainTableLayoutPanel.SuspendLayout();
             this.settingsPanel.SuspendLayout();
@@ -62,6 +64,8 @@ namespace Predator_Prey_Simulation {
             this.settingsPanel.Controls.Add(this.resetButton);
             this.settingsPanel.Controls.Add(this.startButton);
             this.settingsPanel.Controls.Add(this.settingsButton);
+            this.settingsPanel.Controls.Add(this.separatorLine2);
+            this.settingsPanel.Controls.Add(this.separatorLine1);
             this.settingsPanel.Controls.Add(this.textBox9);
             this.settingsPanel.Controls.Add(this.label9);
             this.settingsPanel.Controls.Add(this.textBox8);
@@ -121,7 +125,7 @@ namespace Predator_Prey_Simulation {
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(204, 304);
+            this.textBox9.Location = new System.Drawing.Point(204, 330);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(168, 20);
             this.textBox9.TabIndex = 3;
@@ -130,7 +134,7 @@ namespace Predator_Prey_Simulation {
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(204, 284);
+            this.label9.Location = new System.Drawing.Point(204, 310);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(67, 13);
             this.label9.TabIndex = 4;
@@ -138,7 +142,7 @@ namespace Predator_Prey_Simulation {
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(20, 304);
+            this.textBox8.Location = new System.Drawing.Point(20, 330);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(168, 20);
             this.textBox8.TabIndex = 5;
@@ -147,7 +151,7 @@ namespace Predator_Prey_Simulation {
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(20, 284);
+            this.label8.Location = new System.Drawing.Point(20, 310);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(67, 13);
             this.label8.TabIndex = 6;
@@ -155,7 +159,7 @@ namespace Predator_Prey_Simulation {
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(204, 238);
+            this.textBox7.Location = new System.Drawing.Point(204, 264);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(168, 20);
             this.textBox7.TabIndex = 7;
@@ -164,7 +168,7 @@ namespace Predator_Prey_Simulation {
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(204, 218);
+            this.label7.Location = new System.Drawing.Point(204, 244);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(67, 13);
             this.label7.TabIndex = 8;
@@ -172,7 +176,7 @@ namespace Predator_Prey_Simulation {
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(20, 238);
+            this.textBox6.Location = new System.Drawing.Point(20, 264);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(168, 20);
             this.textBox6.TabIndex = 9;
@@ -181,7 +185,7 @@ namespace Predator_Prey_Simulation {
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(20, 218);
+            this.label6.Location = new System.Drawing.Point(20, 244);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(67, 13);
             this.label6.TabIndex = 10;
@@ -189,7 +193,7 @@ namespace Predator_Prey_Simulation {
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(204, 172);
+            this.textBox5.Location = new System.Drawing.Point(204, 185);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(168, 20);
             this.textBox5.TabIndex = 11;
@@ -198,7 +202,7 @@ namespace Predator_Prey_Simulation {
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(204, 152);
+            this.label5.Location = new System.Drawing.Point(204, 165);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(67, 13);
             this.label5.TabIndex = 12;
@@ -206,7 +210,7 @@ namespace Predator_Prey_Simulation {
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(20, 172);
+            this.textBox4.Location = new System.Drawing.Point(20, 185);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(168, 20);
             this.textBox4.TabIndex = 13;
@@ -215,7 +219,7 @@ namespace Predator_Prey_Simulation {
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 152);
+            this.label4.Location = new System.Drawing.Point(20, 165);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(67, 13);
             this.label4.TabIndex = 14;
@@ -223,7 +227,7 @@ namespace Predator_Prey_Simulation {
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(204, 106);
+            this.textBox3.Location = new System.Drawing.Point(204, 119);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(168, 20);
             this.textBox3.TabIndex = 15;
@@ -232,7 +236,7 @@ namespace Predator_Prey_Simulation {
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(204, 86);
+            this.label3.Location = new System.Drawing.Point(204, 99);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 13);
             this.label3.TabIndex = 16;
@@ -240,7 +244,7 @@ namespace Predator_Prey_Simulation {
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(20, 106);
+            this.textBox2.Location = new System.Drawing.Point(20, 119);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(168, 20);
             this.textBox2.TabIndex = 17;
@@ -249,7 +253,7 @@ namespace Predator_Prey_Simulation {
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 86);
+            this.label2.Location = new System.Drawing.Point(20, 99);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 18;
@@ -288,6 +292,22 @@ namespace Predator_Prey_Simulation {
             this.label0.Size = new System.Drawing.Size(67, 13);
             this.label0.TabIndex = 22;
             this.label0.Text = "Parameter 0:";
+            // 
+            // separatorLine1
+            // 
+            this.separatorLine1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.separatorLine1.Location = new System.Drawing.Point(21, 86);
+            this.separatorLine1.Name = "separatorLine1";
+            this.separatorLine1.Size = new System.Drawing.Size(349, 1);
+            this.separatorLine1.TabIndex = 23;
+            // 
+            // separatorLine2
+            // 
+            this.separatorLine2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.separatorLine2.Location = new System.Drawing.Point(21, 231);
+            this.separatorLine2.Name = "separatorLine2";
+            this.separatorLine2.Size = new System.Drawing.Size(349, 1);
+            this.separatorLine2.TabIndex = 24;
             // 
             // graphicsPanel
             // 
@@ -340,5 +360,7 @@ namespace Predator_Prey_Simulation {
         private System.Windows.Forms.Button settingsButton;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Panel separatorLine1;
+        private System.Windows.Forms.Panel separatorLine2;
     }
 }
